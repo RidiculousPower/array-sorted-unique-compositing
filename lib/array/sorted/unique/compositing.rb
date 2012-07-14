59f997fd2487ff7963e82214177b4135887f6d34
Asher <asher@ridiculouspower.com>
2012-07-14 07:39:49 -0400

    removed commented require_relative since we have Development gem now

--- lib/array/sorted/unique/compositing.rb
+++ lib/array/sorted/unique/compositing.rb
@@ -1,8 +1,6 @@
 
 require 'array-sorted-compositing'
 require 'array-unique-compositing'
-#require_relative '../../../../../array-sorted-compositing/lib/array-sorted-compositing.rb'
-#require_relative '../../../../../array-unique-compositing/lib/array-unique-compositing.rb'
 
 # namespaces that have to be declared ahead of time for proper load order
 require_relative './namespaces'
